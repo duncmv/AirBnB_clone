e13b28b546f07e4fb495be6941221069a3547371
Michael Iyke <ymichaelc@gmail.com>
2024-01-08 17:29:43 +0100

    task 4: Use **kwargs dict to create an instance

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,21 @@ import uuid
 class BaseModel:
     """will be the base model for all others"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """BaseModel constructor"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            return
+
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+            if key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.fromisoformat(value))
+                continue
+            setattr(self, key, value)
 
     def __str__(self):
         """str representation of the object"""
