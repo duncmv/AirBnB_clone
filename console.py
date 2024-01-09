674d2e8034ffea04a47338dffebcdda2e2a9f3c3
Michael Iyke <ymichaelc@gmail.com>
2024-01-09 02:14:22 +0100

    Task6 - working code

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""The Console module: for interacting with the application backend"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """The HBNB command interpreter class"""
+    prompt = "(hbnb) "
+
+    def emptyline(self) -> bool:
+        return False
+
+    def do_quit(self, arg):
+        """Quits the interpreter: QUIT"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quits the interpreter: EOF"""
+        print()
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
