f1020cbad919a42914e749554b4b2a283ae7c86e
Michael Iyke <ymichaelc@gmail.com>
2024-01-10 18:34:55 +0100

    Task 11 - handle User.all on the console
    Handled both User.all which displays all User instances and also the help User.all part of it

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """The Console module: for interacting with the application backend"""
 import cmd
+import os
 from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -40,14 +41,49 @@ class HBNBCommand(cmd.Cmd):
     modelnames = ('Amenity', 'BaseModel', 'City', 'Place',
                   'Review', 'State', 'User')
 
+    def do_help(self, arg: str) -> bool:
+        """Overrides the default help to provide help for
+        the extra or secondary commands. If no such help is found,
+        the default is made to proceed by returning True.
+        This means that search helper functions must return True
+        to make do_help know this."""
+        if arg.lower().startswith("user"):
+            ret = User.user_help(arg)
+            if type(ret) is bool and ret is True:
+                return super().do_help(arg)
+            return ret
+        return super().do_help(arg)
+
+    def get_other_handler(self, line):
+        """Returns the handler of a secondary command if available"""
+        sec_cmds = SecondaryCommands()
+        command = line.split(".")[0].lower()
+        if hasattr(sec_cmds, command):
+            return getattr(sec_cmds, command)
+        return None
+
+    def default(self, line: str) -> bool:
+        """Overrides the default to allow/support extra commands.
+        If te override handler returns True, it means that no proper handler
+        for found for the input. So returning True tells default to proceed
+        execution to the super class' default handling"""
+        handler = self.get_other_handler(line)
+        if handler and callable(handler):
+            ret = handler(line)
+            if type(ret) is bool and ret is True:  # Proceed to default action
+                return super().do_help(line)
+            return ret
+        return super().do_help(line)
+
     def emptyline(self) -> bool:
+        """Overrides the default to prevent re-run of last command"""
         return False
 
     def do_quit(self, arg):
         """Quits the interpreter: QUIT"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, arg: str):
         """Quits the interpreter: EOF"""
         print()
         return True
@@ -130,6 +166,32 @@ on class name"""
             else:
                 setattr(objs[f"{args[0]}.{args[1]}"], args[2], args[3])
                 storage.save_changes(objs)
+
+
+class SecondaryCommands:
+    """The SecondaryCommands class: for other commands command"""
+
+    def exit(self, arg):
+        """Another way to quit: EXIT"""
+        return True
+
+    def cls(self, arg):
+        """Clear the screen if you will: CLS"""
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+    def user(self, arg):
+        """User related commands: User.all"""
+
+        array = arg.split(".")
+
+        # Method exists on User? Then call it with any arguments given
+        if (callable(len(array) > 1 and getattr(User, array[1], None))):
+            getattr(User, array[1], None)(".".join(array[2:]))
+            return
+        return True  # Proceed to the super class' default handling
 
 
 if __name__ == "__main__":
--- models/user.py
+++ models/user.py
@@ -9,3 +9,30 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def user_help(arg):
+        """Utilizes the docstring of User methods to provide help
+        on the command interpreter: HELP USER.ALL, HELP USER
+
+        If no matching method is found, returns True to proceed to
+        proceed to the default action
+        """
+
+        arr = arg.split(".")  # args[0] is User
+        if len(arr) >= 2:
+            meth = getattr(User, arr[1], None)
+            if not meth:
+                return True  # This lets default action proceed
+            print(getattr(User, arr[1]).__doc__)
+        else:
+            print(User.__doc__)
+
+    @staticmethod
+    def all(arg):
+        """Retrieve all users and print them"""
+        from models import storage
+
+        objs = storage.all()
+        for key, value in objs.items():
+            if key.lower().startswith("user"):
+                print(value)
